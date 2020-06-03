5b9bdb731acfe6370c252e547d1e3f6d67788e8d
mdmaruf43 <marufahmedcse76@gmail.com>
2020-06-04 04:36:58 +0600

    some api added

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = process.env.DB_PATH;
+let client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.get('/category', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("category");
+        collection.find().toArray((err, documents) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+app.get('/allMembershipInformation', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("membershipInformation");
+        collection.find().toArray((err, documents) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+});
+
+
+app.post('/addProduct', (req, res) => {
+    //save to database
+    const product = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("category");
+        collection.insert(product, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
+})
+
+app.post('/membershipInformation', (req, res) => {
+    const information = req.body;
+    information.appointmentTime = new Date();
+    console.log(information);
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("powerXgymStore").collection("membershipInformation");
+        collection.insert(information, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(result.ops[0])
+            }
+        });
+        client.close();
+    });
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listing to port 4000'));
